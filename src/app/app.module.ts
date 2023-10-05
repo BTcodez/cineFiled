3e5d66a380679a0ee40d96794dfbb2e2d6bf84c2
Brian Tuohy <BTcodez22@gmail.com>
2023-10-04 19:14:11 -0500

    upd sbar
    attempting to make sidebar responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { FilmComponent } from './film/film.component';
+import { SharedComponent } from './shared/shared.component';
+import { MyWatchlistComponent } from './watchlist/my-watchlist/my-watchlist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    WatchlistComponent,
+    FilmComponent,
+    SharedComponent,
+    MyWatchlistComponent
 
   ],
   imports: [
